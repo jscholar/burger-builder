cded41b6a29932438d77e47eca67d987755f99c5
jscholar <jinruncai@gmail.com>
2019-02-16 11:28:04 -0800

    Optimized OrderSummary view for small width devices

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -20,7 +20,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
-            <h3>Total Price:    ${props.price.toFixed(2)}</h3>
+            <h3>Total Price: ${props.price.toFixed(2)}</h3>
             <p>Continue to Checkout?</p>
             <Button clickHandler={props.cancelPurchase} btnType={'Danger'}>CANCEL</Button>
             <Button clickHandler={props.continuePurchase} btnType={'Success'}>CONTINUE</Button>
