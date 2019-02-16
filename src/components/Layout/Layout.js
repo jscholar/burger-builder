26e0bed7b727090f0f1b0d13a8664951b8c0fcb1
jscholar <jinruncai@gmail.com>
2019-02-16 11:21:00 -0800

    Added SideDrawer toggle functionality. Optimized for mobile.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,20 +6,45 @@ import Toolbar from './../Navigation/Toolbar/Toolbar'
 import SideDrawer from './../Navigation/SideDrawer/SideDrawer'
 import Aux from './../../hoc/Auxiliary';
 
-const layout = (props) => (
-    <Aux>
-        <div>
-            TODO: 
-            Toolbar,
-            SideDrawer,
-            Backdrop
-        </div>
-        <SideDrawer />
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends React.Component {
+    state = {
+        displaySideDrawer: false
+    }
 
-export default layout;
+    closeSideDrawerHandler = () => {
+        this.setState({
+            displaySideDrawer: false
+        })
+    }
+
+    toggleSideDrawerHandler = () => {
+        this.setState((prevState) => {
+            return {
+                displaySideDrawer: !prevState.displaySideDrawer
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <div>
+                    TODO: 
+                    Toolbar,
+                    SideDrawer,
+                    Backdrop
+                </div>
+                <SideDrawer 
+                    display={this.state.displaySideDrawer}
+                    toggle={this.toggleSideDrawerHandler}
+                />
+                <Toolbar toggleSideDrawer={this.toggleSideDrawerHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,16 +2,20 @@ import React from 'react';
 
 import Logo from './../../Logo/Logo'
 import NavigationItems from './../NavigationItems/NavigationItems'
+import SideDrawerBtn from './../SideDrawer/SideDrawerBtn/SideDrawerBtn'
 
 import classes from './Toolbar.module.css'
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div className={classes.ToolbarElement}>MENU</div>
-        <div className={classes.Logo}>
+        <div className={classes.MobileOnly}>
+            <SideDrawerBtn toggleSideDrawer={props.toggleSideDrawer}></SideDrawerBtn>
+        </div>
+
+        <div className={[classes.Logo, classes.DesktopOnly].join(' ')}>
             <Logo />
         </div>
-        <nav className={[classes.ToolbarElement, classes.DesktopOnly].join(' ')}>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
