457fdd87b6edd8ab9a784dc31c7a62fd13ea7591
jscholar <jinruncai@gmail.com>
2019-02-15 19:11:54 -0800

    Cleaned code from order-summary branch

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Burger from './../../components/Burger/Burger'
 import BuildControls from './../../components/Burger/BuildControls/BuildControls'
-import Modal from './../../components/UI/Modal/Modal'
 
 import Aux from '../../hoc/Auxiliary'
 
