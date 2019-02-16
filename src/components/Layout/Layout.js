d31aa1cf54ee2ed42589ec2c6d968b84d35f2c58
jscholar <jinruncai@gmail.com>
2019-02-15 22:25:37 -0800

    Added toolbar with logo and navmenu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,19 @@
 import React from 'react';
+
 import classes from './Layout.module.css';
 
+import Toolbar from './../Navigation/Toolbar/Toolbar'
 import Aux from './../../hoc/Auxiliary';
 
 const layout = (props) => (
     <Aux>
+        <div>
+            TODO: 
+            Toolbar,
+            SideDrawer,
+            Backdrop
+        </div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
