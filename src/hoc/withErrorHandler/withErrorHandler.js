f4a64a74dc833f5706ebc65c7f3c2eae143cd726
jscholar <jinruncai@gmail.com>
2019-03-25 22:41:27 -0700

    fixed issue where modal would not appear with error handler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -14,16 +14,25 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
 
-        componentDidMount() {
+        componentWillMount() {
             axios.interceptors.request.use(req => {
-                this.setState({error: null});
+                this.setState({
+                    error: null
+                });
                 return req;
             })
             axios.interceptors.response.use(res => res, error => {
-                this.setState({error: error});
-                error.error = true;
+                console.log(error);
+                this.setState({
+                    error: error
+                });
                 return error;
             });
+        }
+        
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
