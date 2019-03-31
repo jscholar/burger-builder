cd9f67cd74579ffbbcc3f1489a9b6667ef29cc40
jscholar <jinruncai@gmail.com>
2019-03-31 11:16:03 -0700

    Added disable functionality to button component

--- src/components/UI/Buttons/Button.js
+++ src/components/UI/Buttons/Button.js
@@ -3,7 +3,10 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const button = (props) => (
-    <button className={[classes.Button, classes[props.btnType]].join(' ')} onClick={props.clickHandler}>
+    <button 
+        className={[classes.Button, classes[props.btnType]].join(' ')}
+        disabled={props.disabled}
+        onClick={props.clickHandler}>
         {props.children}
     </button>
 );
